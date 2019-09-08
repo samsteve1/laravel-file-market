fb3f834752f35701809c082de684d8eaa0e3b4da
Steve Idowu <idowusteve@ymail.com>
2019-09-08 23:46:58 +0100

    ldkdk

--- resources/views/home/side-bar.blade.php
+++ resources/views/home/side-bar.blade.php
@@ -1,11 +1,11 @@
 <div class="list is-hoverable is-danger">
-    {{-- <a href="{{ route('popular') }}" class="list-item {{ return_if(on_page('popular'), 'is-active') }}">Popular files</a> --}}
-    {{-- <a href="#" class="list-item">Best selling</a> --}}
+    <a href="{{ route('popular') }}" class="list-item {{ return_if(on_page('popular'), 'is-active') }}">Popular files</a>
+    <a href="#" class="list-item">Best selling</a>
     <br>
     <p><strong>Categories</strong></p>
 
     @foreach ($categories as $category)
-        {{-- <a href="{{ route('category', $category) }}" class="list-item {{ return_if(on_page('categories/'. $category->id), ' is-active') }}">{{ ucfirst($category->name) }}</a> --}}
+        <a href="{{ route('category', $category) }}" class="list-item {{ return_if(on_page('categories/'. $category->id), ' is-active') }}">{{ ucfirst($category->name) }}</a>
     @endforeach
 </div>
 
