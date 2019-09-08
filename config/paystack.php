b3c3ce67bdb286eae4eea19ee0fa5a1cfeecd638
Steve Idowu <idowusteve@ymail.com>
2019-09-08 20:07:47 +0100

    paystack params

--- config/paystack.php
+++ config/paystack.php
@@ -15,24 +15,24 @@ return [
      * Public Key From Paystack Dashboard
      *
      */
-    'publicKey' => env('PAYSTACK_PUBLIC_KEY'),
+    'publicKey' => env('PAYSTACK_PUBLIC_KEY', 'pk_test_861768aad573be79b9b34aa6673670d0bfe0bcb6'),
 
     /**
      * Secret Key From Paystack Dashboard
      *
      */
-    'secretKey' => env('PAYSTACK_SECRET_KEY'),
+    'secretKey' => env('PAYSTACK_SECRET_KEY', 'sk_test_bec9826ca933b1c7b4f841c4c1cd3a1d44fa3b35'),
 
     /**
      * Paystack Payment URL
      *
      */
-    'paymentUrl' => env('PAYSTACK_PAYMENT_URL'),
+    'paymentUrl' => env('PAYSTACK_PAYMENT_URL', 'https://api.paystack.co'),
 
     /**
      * Optional email address of the merchant
      *
      */
-    'merchantEmail' => env('MERCHANT_EMAIL'),
+    'merchantEmail' => env('MERCHANT_EMAIL', 'support@filemarket.com'),
 
 ];
